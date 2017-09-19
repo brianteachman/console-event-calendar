e63fdb083cbef2e022f828191f87722ab480bb3b
unknown <bteachman@STARFLEET.LOCAL>
2017-09-19 10:53:21 -0700

    Added first day lab.

---
+++ HelloWord.java
@@ -0,0 +1,27 @@
+/*
+   Brian Teachman
+   CS140
+   Whatcom Community College
+   9/9/2017
+   helloWorld.java
+   
+   First Java file!
+*/
+ class HelloWord {
+   /*
+      Main function runs here
+   */
+   // purple: reserved words
+   public static void main(String[] args) {
+      /*
+         public: open to outside world
+         static: doesn't require data to run
+      */
+      // .out is a print stream
+      System.out.println("Hello world!");
+      System.out.println("Brian");
+   }
+   
+   public static void hello() {
+   }
+ }
