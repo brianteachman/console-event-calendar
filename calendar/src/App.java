71d2c1fbc74231d61cb325881ff80163f14fe03e
mrtea <mr.teachman@gmail.com>
2017-10-02 17:13:15 -0700

    Readied Calendar Assignment Project.

--- calendar/src/App.java
+++ calendar/src/App.java
@@ -1,6 +1,6 @@
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Your calandar.");
+        System.out.println("Your calandar, sir.");
     }
 }
--- calendar/test/CalendarTests.java
+++ calendar/test/CalendarTests.java
@@ -1,7 +1,7 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
-class AppTests {
+class CalendarTests {
 
     @Test
     void myFirstTest() {
