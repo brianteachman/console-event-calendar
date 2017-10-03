1f96cc66a3ed9e98a4d4ecc8ef95203b812c4017
mrtea <mr.teachman@gmail.com>
2017-10-02 23:07:46 -0700

    Added implementation psuedocode.

--- calendar/src/Calendar.java
+++ calendar/src/Calendar.java
@@ -1,2 +1,60 @@
+/*
+    Brian Teachman
+    CS 140: Whatcom Community College
+    10/2/2017
+
+    Prints a text formatted calendar to the console.
+*/
 public class Calendar {
+
+    public static final int WIDTH = 10;
+
+    /*
+    * Accepts an integer representing the month and displays
+    * the month as a text formatted calendar
+    *
+    * @param int month
+    * @return void
+    * @exit Prints month to stream
+    */
+    public static void drawMonth(int month) {
+        // uses drawRow()
+    }
+
+    /*
+    * Displays one week on the calendar (one row).
+    *
+    * @param int row  Integer representing which row to display
+    * @return void
+    * @exit Prints one week (1-4) of the month to stream
+    */
+    public static void drawRow(int row) {
+
+    }
+
+    /*
+    * Display the date information as a graphical representation of the calendar.
+    *
+    * @param int month
+    * @param int day
+    */
+    public static void displayDate(int month, int day) {
+
+    }
+
+    /*
+    * Given a date as a String, extract an integer value for the month and return it.
+    *
+    * the indexOf and substring methods may be helpful with this
+    */
+    public static void monthFromDate(String date) {
+
+    }
+
+    /*
+    * Given a date as a String, extract an integer value for the day and return it.
+    */
+    public static void dayFromDate(String date) {
+        // the indexOf and substring methods may be helpful with this
+    }
 }
--- calendar/test/CalendarTests.java
+++ calendar/test/CalendarTests.java
@@ -1,6 +1,8 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
+//http://junit.org/junit5/docs/current/user-guide/
+
 class CalendarTests {
 
     @Test
