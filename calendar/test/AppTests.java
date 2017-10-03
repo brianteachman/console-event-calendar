427d2298b22139df6de0b0b0d3fee6b105586870
mrtea <mr.teachman@gmail.com>
2017-10-02 17:03:47 -0700

    Do it!

---
+++ calendar/test/AppTests.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+class AppTests {
+
+    @Test
+    void myFirstTest() {
+        assertEquals(2, 1 + 1);
+    }
+
+}
