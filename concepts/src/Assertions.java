90a6a85d506b31268239b82bfe1ead8600db0724
unknown <BTEACHMAN@starfleet.local>
2017-11-06 11:22:39 -0800

    Todays lessons.

---
+++ concepts/src/Assertions.java
@@ -0,0 +1,9 @@
+/**
+ * Brian Teachman
+ * CS 140: Whatcom Community College
+ * 11/6/2017
+ * <p>
+ * concepts: PACKAGE_NAME
+ */
+public class Assertions {
+}
---
+++ concepts/src/DemorgansLaw.java
@@ -0,0 +1,11 @@
+/**
+ * Brian Teachman
+ * CS 140: Whatcom Community College
+ * 11/6/2017
+ *
+ * Concept: Demorgan's Law
+ *
+ * !(a && b) == !a || !b
+ */
+public class DemorgansLaw {
+}
