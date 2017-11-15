a601422e0987c44f4e9cc0445643d5e36835371f
mrtea <mr.teachman@gmail.com>
2017-11-15 11:08:09 -0800

    Completed lab 6.

--- labs/lab6/src/Lab6.java
+++ labs/lab6/src/Lab6.java
@@ -1,30 +1,17 @@
-import java.io.BufferedReader;
+/* ------------------------------------------------------------------
+ * Brian Teachman
+ * CS 140: Whatcom Community College
+ * 11/15/2017
+ *
+ * Lab 6: File parsing and creating
+ ------------------------------------------------------------------ */
+
 import java.io.File;
-import java.io.FileReader;
 import java.io.PrintStream;
 import java.util.Random;
 import java.util.Scanner;
 
-/**
- * Brian Teachman
- * CS 140: Whatcom Community College
- * 11/14/2017
- *
- * lab6: File parsing and creating
- */
 public class Lab6 {
-    /*
-    Step 1:  Create a program that will generate 1000 random Strings of 7
-    length and place them into one file. The file that this program will
-    create is to be the data source for the second part.
-
-    Step 2: Read in the file you created in part 1. Sort the strings into
-    three other files. Words beginning with A-G || a-g should be written
-    to the first file. Words H-T || h-t should be written into the second
-    file. Reverse all other words and place them into the third file.
-
-    Print to console the number of words in the longest of the files.
-    */
 
     public static void main(String[] args) throws Exception {
 
@@ -33,18 +20,15 @@ public class Lab6 {
         length and place them into one file. The file that this program will
         create is to be the data source for the second part.
         ------------------------------------------------------------------ */
-        Random rgen = new Random();
+        File rwords = new File("rand_words.txt");
 
         // Step 1
-        File fin = new File("rand_words.txt");
-        PrintStream ps = new PrintStream(fin);
+        PrintStream ps = new PrintStream(rwords);
         int i = 1;
         while (i <= 1000) {
-            ps.print(randomWord(rgen));
-//            System.out.print(randomWord(rgen));
+            ps.print(randomWord());
             if (i % 10 == 0) {
                 ps.println();
-//                System.out.println();
             }
             i++;
         }
@@ -55,7 +39,7 @@ public class Lab6 {
         to the first file. Words H-T || h-t should be written into the second
         file. Reverse all other words and place them into the third file.
         ------------------------------------------------------------------ */
-        Scanner reader = new Scanner(fin);
+        Scanner reader = new Scanner(rwords);
         PrintStream atog = new PrintStream(new File("atog.txt"));
         PrintStream htot = new PrintStream(new File("htot.txt"));
         PrintStream utoz = new PrintStream(new File("utoz.txt"));
@@ -64,60 +48,53 @@ public class Lab6 {
         int sum3 = 0;
         while (reader.hasNext()) {
             String word = reader.next();
-            if (word.toLowerCase().charAt(0) >= 'a'
-                    && word.toLowerCase().charAt(0) <= 'g') {
-                atog.append(word + ' ');
-                sum1++;
+            char firstChar = word.toLowerCase().charAt(0);
+            if (firstChar >= 'a' && firstChar <= 'g') {
+                atog.append(word).append(" ");
+                if (++sum1 % 10 == 0) atog.append("\n");
             }
-            else if (word.toLowerCase().charAt(0) >= 'h'
-                    && word.toLowerCase().charAt(0) <= 't') {
-                htot.append(word + ' ');
-                sum2++;
+            else if (firstChar >= 'h' && firstChar <= 't') {
+                htot.append(word).append(" ");
+                if (++sum2 % 10 == 0) htot.append("\n");
             }
-            else if (word.toLowerCase().charAt(0) >= 'u'
-                    && word.toLowerCase().charAt(0) <= 'z') {
-                String rword = "";
-                for (int j = 0; j < word.length(); j++) {
-                    rword += word.charAt((word.length()-1)-j);
-                }
-                utoz.append(rword + ' ');
-                sum3++;
+            else if (firstChar >= 'u' && firstChar <= 'z') {
+//                String rword = "";
+//                for (int j = 0; j < word.length(); j++) {
+//                    rword += word.charAt((word.length()-1)-j);
+//                }
+                StringBuilder sb = new StringBuilder();
+                utoz.append(sb.append(word).reverse().append(" ").toString());
+                if (++sum3 % 10 == 0) utoz.append("\n");
             }
         }
-
-        System.out.println(sum1 + " " + sum2 + " "+sum3);
-
 
         // Print out which file has the largest count
-        if (sum1 < sum2) {
-            if (sum2 < sum3) {
-                System.out.println("The file utoz.txt has the greatest word count. ("+sum3+" words)");
-            }
-            else {
-                System.out.println("The file htot.txt has the greatest word count. ("+sum2+" words)");
-            }
+        if (sum1 < sum3 && sum2 < sum3) {
+            System.out.println("File utoz.txt has the greatest word count. ("+sum3+" words)");
         }
-        else {
-            if (sum1 > sum3) {
-                System.out.println("The file utot.txt has the greatest word count. ("+sum3+" words)");
-            }
-            else {
-                System.out.println("The file utot.txt has the greatest word count. ("+sum2+" words)");
-            }
+        else if (sum1 < sum2 && sum3 < sum2) {
+            System.out.println("File htot.txt has the largest distribution of random words everytime. ("+sum2+" words)");
+        }
+        else if (sum2 < sum1 && sum3 < sum1) {
+                System.out.println("File atog.txt has the greatest word count. ("+sum1+" words)");
         }
     }
 
-    private static String randomWord(Random rgen) {
+    public static String randomWord() {
         StringBuilder s = new StringBuilder();
+        Random rgen = new Random();
         for (int i=0; i < 7; i++) {
+            int rmod = 1 + rgen.nextInt(5);
             int rchar = rgen.nextInt(26);
-            int rmod = 1+ rgen.nextInt(5);
-            int out ;
-            if (i % rmod == 0) {
-                out = 65 + rchar;
+            // tried to even out distribution, ...still the same shape
+//            int j = rmod;
+//            while (0 < j--) rchar = rgen.nextInt(26);
+            int out;
+            if (i % rmod == 0 || i % rmod == 2) {
+                out = 97 + rchar;
             }
             else {
-                out = 97 + rchar;
+                out = 65 + rchar;
             }
             s.append((char) out);
         }
