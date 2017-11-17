91612bdd9120e22aeb4b563179dd8b0009fc4e15
mrtea <mr.teachman@gmail.com>
2017-11-17 01:00:22 -0800

    Complete refactoring. Can now handle multiple Calendar objects.

--- calendar/src/Application.java
+++ calendar/src/Application.java
@@ -1,184 +1,53 @@
-/*
+/* ----------------------------------------------------------------------------
     Brian Teachman
     CS 140: Whatcom Community College
     10/11/2017
 
-    A console calendar - displays a given month using the CalendarHelper class
- */
+    A text based calendar:
+
+    Displays a given month in text using java.util.Calendar
+ ----------------------------------------------------------------------------*/
 
 import java.util.Scanner;
 
-// https://docs.oracle.com/javase/8/docs/api/java/util/Calendar.html
-
 public class Application extends PrintCandy {
 
-    final static int CELL_WIDTH = 5;
-
-    private static CalendarHelper helper;
-    private static Scanner input;
-    private static boolean dateIsSet = false;
-
-    public static void main(String[] args) {
-
-        helper = new CalendarHelper();
-        input = new Scanner(System.in); // user prompt
-
-//        stupidASCIIArt();
-
-        boolean running = true;
-        while (running) {
-            switch (menu()) {
-                case 'e':
-                    store(promptForDate());
-                    drawMonth();
-                    break;
-
-                case 't':
-                    store(helper.getMonth()+"/"+helper.getDay());
-                    drawMonth();
-                    break;
-
-                case 'n':
-                    if (dateIsSet) {
-                        helper.nextMonth();
-                        drawMonth();
-                    }
-                    else {
-                        println("\nYou need to have a calendar displayed first.");
-                    }
-                    break;
-
-                case 'p':
-                    if (dateIsSet) {
-                        helper.previousMonth();
-                        drawMonth();
-                    }
-                    else {
-                        println("\nYou need to have a calendar displayed first.");
-                    }
-                    break;
-
-                case 'q':
-                    running = false;
-                    break;
-
-                default:
-                    println("Please enter a valid command.");
-                    break;
-            }
-//            println("Start of the month: " + helper.getDayMonthStartsOn());
-//            println("End of the month:   " + helper.getLastDayofMonth());
-        }
-
-    }
-
-    /**
-     * @return char  The selected menu item. ("e", "t", "n", "p", or "q")
-     */
-    public static char menu() {
-        println("\nPlease type a command");
-        println("    \"e\" to enter a date and display the corresponding");
-        println("    \"t\" to get todays date and display today's calendar");
-        println("    \"n\" to display the next month");
-        println("    \"p\" to display the previous month");
-        println("    \"q\" to quit the program");
-        String command = input.nextLine();
-        if (command.length() > 0) {
-            return command.charAt(0);
-        }
-        return 'x';
-    }
-
-    /**
-     * @return String  Returns a date string in the format mm/dd
-     */
-    public static String promptForDate() {
-        print("What date would you like like to look at? (mm/dd): ");
-        return input.nextLine(); // in-line processing
-    }
-
-    private static void store(String inputData) {
-        try {
-            setCalendarData(inputData);
-        }
-        catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            store(promptForDate()); // fail gracefully (recursive reprompt)
-        }
-        dateIsSet = true;
-    }
-
-    /**
-     * Validates and sets users input
-     *
-     * Check that month (mm) is between 1 and 12, day (dd) is between 1 and 31,
-     * and that they are formatted correctly (mm/dd).
-     *
-     * @param inputData String
-     * @throws IllegalArgumentException
-     */
-    public static void setCalendarData(String inputData) throws IllegalArgumentException {
-        int delimiterIndex = helper.getDelimiterIndex(inputData);
-        // TODO: move date formatting validation into it's mutator method?
-        //       trying to keep view (the error message) data in the Application class
-        //       and passing the error message through two method calls seems extreme
-        if ( ! (delimiterIndex == 1 || delimiterIndex == 2)) { // accounts for '1' or '01' formatted case
-            throw new IllegalArgumentException("Expected the format 'mm/dd', where mm is the month and dd is the day.");
-        }
-        helper.setCalendarDate(inputData);
-    }
-
+    private final static int CELL_WIDTH = 5;
     private static int dayCount = 0; // for tracking days in printCalendarDay
-    // TODO: refactor this global
-    /*
-     * Accepts an integer representing the month and displays
-     * the month as a text formatted calendar
-     *
-     * @param int month
-     * @param void
-     */
-    static public void drawMonth() {
-        println("\n"+helper.getDateLong());
+
+    // TODO: refactor these out of global space
+    private static String lastDateIn;
+    private static Integer delimiterIndex; // using Integer for nullability
+
+    public static void displayDate(int month, int day) {
+        println("\nMonth: " + month);
+        println("Day:   " + day);
+    }
+
+    // Accepts an integer representing the month and displays
+    // the month as a text formatted calendar
+    public static void drawMonth(BtCalendar c) {
+        println("\n"+c.getMonthName());
         rowHeader(CELL_WIDTH);
         for (int i = 0; i < 5; i++) { // 5 weeks
-            drawRow(i);
+            drawRow(c, i);
             rowHeader(CELL_WIDTH);
         }
-        displayDate(helper.getMonth(), helper.getDay());
+        displayDate(c.getMonth(), c.getDay());
         dayCount = 0;
     }
 
-    /**
-     * Print a row divider, some given width
-     *
-     * @param width
-     * @return void
-     */
-    public static void rowHeader(int width) {
-        for (int i = 0; i < 7; i++) {
-            for (int j = 0; j < width; j++) {
-                print("=");
-            }
-        }
-        print("=\n");
-    }
-
-    /*
-     * Displays one week on the calendar (one row).
-     *
-     * @param int row  Integer representing which row to display
-     * @return void
-     */
-    public static void drawRow(int row) {
+    // Print one week of the calendar (one row) to the stream
+    public static void drawRow(BtCalendar c, int row) {
         for (int rowHeight = 0; rowHeight < cellHeight(); rowHeight++) { // cell height number of rows
             print("|"); // start row
             for (int cellNumber = 0; cellNumber < 7; cellNumber++) { // 7 cells for 7 days
                 for (int k = 0; k < CELL_WIDTH; k++) { // cell content
                     if (k == 1 && rowHeight == 0) { // after one blank space on the first row
 
-                        printCalendarDay(row, cellNumber);
-                        if (helper.isCurrentDay(dayCount)) {
-                            print("*"); //TODO: mark the current day
+                        printCalendarDay(c, row, cellNumber);
+                        if (c.isCurrentDay(dayCount)) {
+                            print("*");
                             k += 2; // account for double digit format
                         }
                         else {
@@ -196,13 +65,19 @@ public class Application extends PrintCandy {
         }
     }
 
-    /**
-     * Let the row height be 1/2 * the row width, unless the cell's width
-     * is less than or equal to 4, than it is 1.
-     *
-     * @return int
-     */
-    public static int cellHeight() {
+    // Print a row divider, some given width
+    private static void rowHeader(int width) {
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < width; j++) {
+                print("=");
+            }
+        }
+        print("=\n");
+    }
+
+    // Let the row height be 1/2 * the row width, unless the cell's width
+    // is less than or equal to 4, than it is 1.
+    private static int cellHeight() {
         int rowHieght = CELL_WIDTH/2;
         if (rowHieght < 1 || CELL_WIDTH == 4) {
             rowHieght = 1;
@@ -211,17 +86,13 @@ public class Application extends PrintCandy {
         return rowHieght;
     }
 
-    /**
-     *
-     * @param rowNumber
-     * @param cellNumber
-     * return void
-     */
-    public static void printCalendarDay(int rowNumber, int cellNumber) {
-        int startWeekDay = helper.getDayMonthStartsOn();
-        int daysInMonth = helper.getLastDayofMonth();
+    // Print cell data to stream
+    private static void printCalendarDay(BtCalendar c, int rowNumber, int cellNumber) {
+        int startWeekDay = c.getFirstWeekdayOfMonth();
+        int daysInMonth = c.getLastDayOfMonth();
 
-        if ((rowNumber == 0 && cellNumber < (startWeekDay - 1)) || dayCount >= daysInMonth) {
+        if ((rowNumber == 0 && cellNumber < (startWeekDay - 1))
+                || dayCount >= daysInMonth) {
             print("  ");
         }
         else if (dayCount < daysInMonth) {
@@ -229,7 +100,6 @@ public class Application extends PrintCandy {
                 print(" ");
             }
             print(++dayCount);
-//            print(new DecimalFormat("00").format(++dayCount));
 //            print("%2d", ++dayCount);
         }
         else {
@@ -237,18 +107,66 @@ public class Application extends PrintCandy {
         }
     }
 
-    /**
-     *
-     * @param month
-     * @param day
-     * @return void
-     */
-    public static void displayDate(int month, int day) {
-        println("\nMonth: " + month);
-        println("Day:   " + day);
+    private static void drawCurrentMonth(BtCalendar c) {
+        if (c == null) {
+            c = new BtCalendar();
+        }
+        drawMonth(c);
     }
 
-    public static void stupidASCIIArt() {
+    // The selected menu item. ("e", "t", "n", "p", or "q")
+    private static char menu(Scanner input) {
+        println("\nPlease type a command");
+        println("    \"e\" to enter a date and display the corresponding");
+        println("    \"t\" to get todays date and display today's calendar");
+        println("    \"n\" to display the next month");
+        println("    \"p\" to display the previous month");
+        println("    \"q\" to quit the program");
+        print("> ");
+        String command = input.nextLine();
+        if (command.length() > 0) {
+            return command.charAt(0);
+        }
+        return 'x';
+    }
+
+    // Returns a date string in the format mm/dd
+    private static String promptForDate(Scanner input) {
+        print("What date would you like like to look at? (mm/dd): ");
+        return input.nextLine(); // in-line processing
+    }
+
+    private static void setDateFromPrompt(BtCalendar c, Scanner input) {
+        String formattedDate = promptForDate(input); // in-line processing
+        try {
+            int delimiterIndex = getDelimiterIndex(formattedDate);
+            int month = Integer.valueOf(formattedDate.substring(0, delimiterIndex));
+            int day = Integer.valueOf(formattedDate.substring(delimiterIndex+1));
+            println(month + "/" + day);
+            c.setDate(month, day, 2017);
+        }
+        catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            setDateFromPrompt(c, input); // fail gracefully (recursive reprompt)
+        }
+    }
+
+    private static int getDelimiterIndex(String formattedDate) {
+        if (delimiterIndex == null || !lastDateIn.equals(formattedDate)) { // only iterate through the string once
+            setDelimiterIndex(formattedDate);
+            lastDateIn = formattedDate;
+        }
+        return delimiterIndex;
+    }
+
+    private static void setDelimiterIndex(String formattedDate) {
+        delimiterIndex = formattedDate.indexOf("/");
+        if ( ! (delimiterIndex == 1 || delimiterIndex == 2)) { // accounts for '1' or '01' formatted case
+            throw new IllegalArgumentException("Expected the format 'mm/dd', where mm is the month and dd is the day.");
+        }
+    }
+
+    private static void stupidASCIIArt() {
         int rows = 10;
         int cols = CELL_WIDTH * 7;
         for (int i=0; i < rows; i++) {
@@ -277,4 +195,63 @@ public class Application extends PrintCandy {
         print("\n\n");
     }
 
+    /*----------------------------------------------------------------------
+    * Calendar client
+    ----------------------------------------------------------------------*/
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); // user prompt
+        BtCalendar delta = new BtCalendar();
+        BtCalendar current = null;
+
+        boolean running = true;
+        while (running) {
+            switch (menu(input)) {
+                case 'e':
+                    setDateFromPrompt(delta, input);
+                    // TODO: add ASCII art
+                    drawMonth(delta);
+                    drawCurrentMonth(current);
+                    break;
+
+                case 't':
+                    // TODO: add ASCII art
+                    drawCurrentMonth(current);
+                    delta.setDateSetFlag(true);
+                    break;
+
+                case 'n':
+                    if (delta.isDateSet()) {
+                        delta.nextMonth();
+                        // TODO: add ASCII art
+                        drawMonth(delta);
+                        drawCurrentMonth(current);
+                    }
+                    else {
+                        println("\nYou need to have a calendar displayed first.");
+                    }
+                    break;
+
+                case 'p':
+                    if (delta.isDateSet()) {
+                        delta.previousMonth();
+                        // TODO: add ASCII art
+                        drawMonth(delta);
+                        drawCurrentMonth(current);
+                    }
+                    else {
+                        println("\nYou need to have a calendar displayed first.");
+                    }
+                    break;
+
+                case 'q':
+                    running = false;
+                    println("\nThat's one way to do it, good day.");
+                    break;
+
+                default:
+                    println("Please enter a valid command.");
+                    break;
+            }
+        }
+    }
 }
---
+++ calendar/test/BtCalendarTests.java
@@ -0,0 +1,76 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class BtCalendarTests {
+
+    @Test
+    void testConstructCalendarWithNoParams() {
+        try {
+            BtCalendar cm = new BtCalendar();
+        }
+        catch (Exception e) {
+            assertEquals(true, "Should have worked.");
+        }
+    }
+
+    @Test
+    void testConstructCalendarWithParams() {
+        try {
+            BtCalendar cm = new BtCalendar(11, 15);
+        }
+        catch (Exception e) {
+            assertEquals(true, "Should have worked.");
+        }
+    }
+
+    @Test
+    void testConstructedCalendarHasCorrectDate() {
+        BtCalendar cm = new BtCalendar(11, 15);
+        assertEquals(11, cm.getMonth());
+        assertEquals(15, cm.getDay());
+    }
+
+    @Test
+    void testNextMonthIncrementsOneMonth() {
+        BtCalendar cm = new BtCalendar(1, 25);
+        cm.nextMonth();
+        assertEquals(2, cm.getMonth());
+    }
+
+    @Test
+    void testPreviousMonthFromJanuarySetsDecemberOfPreviousYear() {
+        BtCalendar cm = new BtCalendar(1, 25);
+        cm.previousMonth();
+        assertEquals(12, cm.getMonth());
+        assertEquals(2016, cm.getYear());
+    }
+
+    @Test
+    void testNextMonthFromDecemberSetsJanuaryOfNextYear() {
+        BtCalendar cm = new BtCalendar(12, 25);
+        cm.nextMonth();
+        assertEquals(1, cm.getMonth());
+        assertEquals(2018, cm.getYear());
+    }
+
+    @Test
+    void testGetDayMonthStartsOn() {
+        BtCalendar cm = new BtCalendar(11, 25);
+        assertEquals(4, cm.getFirstWeekdayOfMonth());
+    }
+
+    @Test
+    void testGetDayMonthStartsOnDoesntChangeCalendarDay() {
+        BtCalendar cm = new BtCalendar(11, 25);
+        cm.getFirstWeekdayOfMonth();
+        assertEquals(25, cm.getDay());
+    }
+
+    @Test
+    void testCanGetWeekdayOfBeginningOfMonth() {
+        BtCalendar cm = new BtCalendar(11, 25);
+        assertEquals(4, cm.getFirstWeekdayOfMonth());
+    }
+
+}
