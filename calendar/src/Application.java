99afc55521c942b547fdaf0ec86355afef2ae8e4
mrtea <mr.teachman@gmail.com>
2017-11-17 17:42:54 -0800

    Moved BtCalendar into Application as a private static method. Left module for testing.

--- calendar/src/Application.java
+++ calendar/src/Application.java
@@ -9,13 +9,16 @@
  ----------------------------------------------------------------------------*/
 
 import java.io.File;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Application {
 
     private final static int CELL_WIDTH = 5;
     private static int dayCount = 0; // for tracking days in printCalendarDay
+
+    /*----------------------------------------------------------------------------
+     * Application objects
+     *--------------------------------------------------------------------------*/
 
     private static class Delimiter {
         private static String lastDateIn;
@@ -41,6 +44,123 @@ public class Application {
             if ( ! (delimiterIndex == 1 || delimiterIndex == 2)) { // accounts for '1' or '01' formatted case
                 throw new IllegalArgumentException("Expected the format 'mm/dd', where mm is the month and dd is the day.");
             }
+        }
+    }
+
+    private static class BtCalendar {
+
+        public int[] monthDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        private String[] monthName = { "January", "February", "March", "April", "May", "June", "July",
+                "August", "September", "October", "November", "December" };
+
+        public boolean dateIsSet;
+        private Calendar cal;
+
+        /*----------------------------------------------------------------------------
+         * Construction
+         *--------------------------------------------------------------------------*/
+
+        // This calendar instantiated with given date
+        public BtCalendar(int month, int day) {
+            this.cal = startNewCalendar(month, day);
+            dateIsSet = true; // explicitly set date
+        }
+
+        // This calendar instantiated with todays date
+        public BtCalendar() {
+            this.cal = startNewCalendar();
+            dateIsSet = false; // technically it is set, however, not explicitly
+        }
+
+        // Return Calendar object representing today if month and day are null
+        public Calendar startNewCalendar(Integer month, Integer day) {
+            Calendar c = startNewCalendar();
+            setDate(c, month, day, 2017);
+            return c;
+        }
+
+        public Calendar startNewCalendar() {
+            return Calendar.getInstance(TimeZone.getTimeZone("America/Vancouver"));
+        }
+
+        /*----------------------------------------------------------------------------
+         * Calendar Accessors
+         *--------------------------------------------------------------------------*/
+
+        public int getDay() {
+            return this.cal.get(Calendar.DAY_OF_MONTH);
+        }
+
+        public int getMonth() {
+            return this.cal.get(Calendar.MONTH) + 1;
+        }
+
+        public String getMonthName() {
+            return this.monthName[this.cal.get(Calendar.MONTH)];
+        }
+
+        public int getYear() {
+            return this.cal.get(Calendar.YEAR);
+        }
+
+        // Returns weekday of start of the month (1..7), Sunday through Saturday
+        public int getFirstWeekdayOfMonth() {
+            Calendar copy = (Calendar) this.cal.clone();
+            copy.set(Calendar.DAY_OF_MONTH, 1);
+            return copy.get(Calendar.DAY_OF_WEEK);
+        }
+
+        public int getLastDayOfMonth() {
+            return this.cal.getActualMaximum(Calendar.DAY_OF_MONTH);
+        }
+
+        public boolean isDateSet() {
+            return dateIsSet;
+        }
+
+        public boolean isCurrentDay(int day) {
+            return day == this.getDay();
+        }
+
+        /*----------------------------------------------------------------------------
+         * Calendar Mutators
+         *--------------------------------------------------------------------------*/
+
+        // set the date of the invariant java.util.Calendar
+        public void setDate(int month, int day, int year) {
+            this.cal.set(year, month-1, day);
+            dateIsSet = true;
+        }
+
+        // Set the date of a non-invariant Calendar.
+        public void setDate(Calendar c, int month, int day, int year) {
+            c.set(year, month-1, day);
+            dateIsSet = true;
+        }
+
+        public void setDateSetFlag(boolean flag) {
+            dateIsSet = flag;
+        }
+
+        public void nextMonth() {
+            this.addMonthDelta(this.cal,1);
+        }
+
+        public void previousMonth() {
+            this.addMonthDelta(this.cal,-1);
+        }
+
+        private void addMonthDelta(Calendar cal, int nextMonth) {
+            int thisMonth = this.getMonth() + 1; // convert from index to month number
+            cal.add(Calendar.MONTH, thisMonth - (thisMonth - nextMonth));
+        }
+
+        private void zeroOutTime(Calendar cal) {
+            // set the time to the start of the day 00:00:00
+            cal.set(Calendar.HOUR_OF_DAY, 0); // reset hour, clear doesn't reset the hour of day
+            cal.clear(Calendar.MINUTE);       // reset minutes
+            cal.clear(Calendar.SECOND);       // reset seconds
+            cal.clear(Calendar.MILLISECOND);  // reset millis
         }
     }
 
@@ -225,8 +345,10 @@ public class Application {
         String formattedDate = promptForDate(input); // in-line processing
         try {
             int delimiterIndex = delim.getIndex(formattedDate);
-            int month = Integer.valueOf(formattedDate.substring(0, delimiterIndex));
-            int day = Integer.valueOf(formattedDate.substring(delimiterIndex+1));
+//            int month = Integer.valueOf(formattedDate.substring(0, delimiterIndex));
+            int month = monthFromDate(formattedDate, delimiterIndex);
+//            int day = Integer.valueOf(formattedDate.substring(delimiterIndex+1));
+            int day = dayFromDate(formattedDate, delimiterIndex);
             println(month + "/" + day);
             c.setDate(month, day, 2017);
         }
@@ -234,6 +356,47 @@ public class Application {
             System.out.println(e.getMessage());
             setDateFromPrompt(c, input); // fail gracefully (recursive reprompt)
         }
+    }
+
+    // Given a date as a String, extract an integer value for the day and return it.
+    private static int dayFromDate(String formatedDate, int delimiter) throws IllegalArgumentException {
+        String dd = formatedDate.substring(delimiter+1);
+        return validateAndReturnInt(dd, 31, "Days");
+    }
+
+    // Given a date as a String, extract an integer value for the month and return it.
+    private static int monthFromDate(String formatedDate, int delimiter) throws IllegalArgumentException {
+        String mm = formatedDate.substring(0, delimiter);
+        return validateAndReturnInt(mm, 12, "Months");
+    }
+
+    // Check date segments for valid, correctly formatted input
+    private static int validateAndReturnInt(String dateSegment, int upperBound, String segmentName)
+            throws IllegalArgumentException {
+        dateSegment = removeLeadingZero(dateSegment);
+        ArrayList<String> validSegments = getValidList(upperBound);
+        if ( ! validSegments.contains(dateSegment)) { // check input is in valid month range
+            throw new IllegalArgumentException(String.format("\n%s range from 1 to %d. %s given.\n",
+                    segmentName, upperBound, dateSegment));
+        }
+        return Integer.parseInt(dateSegment);
+    }
+
+    // Build a list of ascii integers representing months and days (for easy range checking)
+    private static ArrayList<String> getValidList(int upperBound) {
+        ArrayList<String> list = new ArrayList<String>();
+        for (int i=1; i<=upperBound; i++) {
+            list.add("".valueOf(i));
+        }
+        return list;
+    }
+
+    // If string segment starts with a zero, remove it
+    private static String removeLeadingZero(String dateSegment) {
+        if (dateSegment.startsWith("0")) {
+            dateSegment = dateSegment.substring(1);
+        }
+        return dateSegment;
     }
 
     /*----------------------------------------------------------------------
